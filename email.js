b4ed5e12e3878660cb73600b74580a6f2f09ab65
mp1erce <mp@michaelepierce.com>
2014-06-14 19:46:26 -0700

    missing semicolon line 21

--- email.js
+++ email.js
@@ -1,36 +1,36 @@
-	function resetForm($form) {
-	  $form.find('input:text,select, textarea').val('');
-	}
+function resetForm($form) {
+  $form.find('input:text,select, textarea').val('');
+}
 
-	$(function() {
-	  $("#contactForm").submit(function() {
+$(function() {
+  $("#contactForm").submit(function() {
 
-	    document.ContactForm.submitf.value = 'Please wait...';
+    document.ContactForm.submitf.value = 'Please wait...';
 
-	    $.post("process.php?send=comments", $("#contactForm").serialize(),
-	      function(data) {
-	        if (data.frm_check == 'error') {
+    $.post("process.php?send=comments", $("#contactForm").serialize(),
+      function(data) {
+        if (data.frm_check == 'error') {
 
-	          $("#message_post").hide().html("<div class='errorMessage'>" + data.msg + "</div>").fadeIn('slow');
-	          document.ContactForm.submitf.value = 'Submit Again';
-	          document.ContactForm.submitf.disabled = false;
-	        } else {
-	          $("#message_post").hide().html("<div class='successMessage'>Talk Soon!</div>").fadeIn(2000).fadeOut(4000);
-	          document.ContactForm.submitf.value = 'Sent. Thank you.';
-	          setTimeout(function() {
-	            $(submitf).fadeIn(500).attr('value', 'Submit')
-	          }, 1200);
-	          resetForm($('#contactForm'));
-	          setTimeout(function() {
+          $("#message_post").hide().html("<div class='errorMessage'>" + data.msg + "</div>").fadeIn('slow');
+          document.ContactForm.submitf.value = 'Submit Again';
+          document.ContactForm.submitf.disabled = false;
+        } else {
+          $("#message_post").hide().html("<div class='successMessage'>Talk Soon!</div>").fadeIn(2000).fadeOut(4000);
+          document.ContactForm.submitf.value = 'Sent. Thank you.';
+          setTimeout(function() {
+            $(submitf).fadeIn(500).attr('value', 'Submit');
+          }, 1200);
+          resetForm($('#contactForm'));
+          setTimeout(function() {
 
-	            $("#contactForm").toggle("fast");
-	          }, 2000);
-	          return false;
+            $("#contactForm").toggle("fast");
+          }, 2000);
+          return false;
 
-	        }
-	      }, "json");
+        }
+      }, "json");
 
-	    return false;
+    return false;
 
-	  });
-	});
+  });
+});
